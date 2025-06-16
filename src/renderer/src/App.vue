0af579be0def8e99b47d60e813b38377ad777f4c
hyh31 <620042827@qq.com>
2025-06-16 13:20:57 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+
+    </SidebarInset>
+  </SidebarProvider>
+</template>
